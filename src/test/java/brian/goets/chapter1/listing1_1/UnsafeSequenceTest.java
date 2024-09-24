f14c3fea5a08df46e7bc9977a8791d57d19c7056
krenMaksim <krenmaba@gmail.com>
2024-09-24 18:32:33 +0300

    in progress with UnsafeSequenceTest

--- src/test/java/brian/goets/chapter1/listing1_1/UnsafeSequenceTest.java
+++ src/test/java/brian/goets/chapter1/listing1_1/UnsafeSequenceTest.java
@@ -1,5 +1,42 @@
 package brian.goets.chapter1.listing1_1;
 
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class UnsafeSequenceTest {
 
+  //
+  //  @Test
+  @RepeatedTest(10)
+  void getNext() {
+    int cores = Runtime.getRuntime().availableProcessors();
+    ExecutorService exec = Executors.newFixedThreadPool(cores);
+
+    var unsafeSequence = new UnsafeSequence();
+
+    Runnable task = () -> {
+      unsafeSequence.getNext();
+      //      System.out.println(Thread.currentThread().getName() + " " + unsafeSequence.getNext());
+    };
+
+    int numberOfIterations = 1_000;
+    IntStream.rangeClosed(1, numberOfIterations).forEach(i -> exec.execute(task));
+
+    exec.shutdown();
+
+    while (!exec.isTerminated()) {
+      // Noop. Waiting
+    }
+
+    //    System.out.println(exec.isTerminated());
+    int result = unsafeSequence.getNext();
+    //    System.out.println(result);
+
+    assertThat(result).isNotEqualTo(numberOfIterations);
+  }
 }
