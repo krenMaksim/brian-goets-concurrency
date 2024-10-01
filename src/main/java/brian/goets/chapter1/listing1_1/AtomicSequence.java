ef551d6a278ec56a5967cb55043cb0062540c9f7
krenMaksim <krenmaba@gmail.com>
2024-10-01 08:22:55 +0300

    Allocated AtomicSequence

---
+++ src/main/java/brian/goets/chapter1/listing1_1/AtomicSequence.java
@@ -0,0 +1,15 @@
+package brian.goets.chapter1.listing1_1;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class AtomicSequence {
+
+  private final AtomicInteger value = new AtomicInteger();
+
+  public int getNext() {
+    return value.getAndIncrement();
+  }
+}
--- src/test/java/brian/goets/chapter1/listing1_1/UnsafeSequenceTest.java
+++ src/test/java/brian/goets/chapter1/listing1_1/UnsafeSequenceTest.java
@@ -58,6 +58,17 @@ class UnsafeSequenceTest {
     assertThat(result).isEqualTo(NUMBER_OF_ITERATIONS);
   }
 
+  @RepeatedTest(10)
+  void doGivenNumberOfIterationsOverAtomicSequence() throws InterruptedException {
+    AtomicSequence atomicSequence = new AtomicSequence();
+
+    submitForExecutionForNumberOfTimes(() -> atomicSequence.getNext(), NUMBER_OF_ITERATIONS);
+    int result = atomicSequence.getNext();
+
+    System.out.println("Result: " + result);
+    assertThat(result).isEqualTo(NUMBER_OF_ITERATIONS);
+  }
+
   // TBD what about using a barrier for blocking
 
   private <T> void submitForExecutionForNumberOfTimes(Callable<T> task, int numberOfIterations) throws InterruptedException {
