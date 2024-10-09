a11dc889fbd679196308a913e316cfb471e0eabf
krenMaksim <krenmaba@gmail.com>
2024-10-09 09:20:29 +0300

    Updated TaskIterator

--- src/test/java/brian/goets/test/util/TaskIterator.java
+++ src/test/java/brian/goets/test/util/TaskIterator.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,16 +13,18 @@ public class TaskIterator {
 
   private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-  public static <T> void submitForExecutionForNumberOfTimes(Callable<T> task, int numberOfIterations) throws InterruptedException {
+  public static <T> List<Future<T>> submitForExecutionForNumberOfTimes(Callable<T> task, int numberOfIterations) throws InterruptedException {
     ExecutorService exec = Executors.newFixedThreadPool(AVAILABLE_PROCESSORS);
 
     List<Callable<T>> tasks = IntStream.rangeClosed(1, numberOfIterations)
         .mapToObj(i -> task)
         .collect(Collectors.toList());
 
-    exec.invokeAll(tasks);
+    List<Future<T>> executedTasks = exec.invokeAll(tasks);
 
     exec.shutdown();
     exec.awaitTermination(30, TimeUnit.SECONDS);
+
+    return executedTasks;
   }
 }
