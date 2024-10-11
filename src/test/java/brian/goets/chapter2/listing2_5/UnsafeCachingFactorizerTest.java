f210dc62de02eec3700368bfd229386f475496cc
krenMaksim <krenmaba@gmail.com>
2024-10-11 16:57:15 +0300

    have something working

--- src/test/java/brian/goets/chapter2/listing2_5/UnsafeCachingFactorizerTest.java
+++ src/test/java/brian/goets/chapter2/listing2_5/UnsafeCachingFactorizerTest.java
@@ -1,11 +1,13 @@
 package brian.goets.chapter2.listing2_5;
 
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.math.BigInteger;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
@@ -14,39 +16,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class UnsafeCachingFactorizerTest {
 
-  private static final int NUMBER_OF_ITERATIONS = 10_000;
+  private static final int NUMBER_OF_ITERATIONS = 100;
 
-  @Test
-  void doSomething() throws IOException {
-    UnsafeCachingFactorizer factorizer = new UnsafeCachingFactorizer();
-    ServletRequest request = ServletHelper.newServletRequestWithFactorNumber(new BigInteger("42"));
-    ServletResponse response = ServletHelper.newServletResponse();
-
-    factorizer.service(request, response);
-    BigInteger[] factors = ServletHelper.extractFactors(response);
-
-    assertThat(factors).isEqualTo(new BigInteger[] {new BigInteger("42")});
-  }
-
-  @RepeatedTest(1)
+  @RepeatedTest(10)
   void doGivenNumberOfRequestsOverUnsafeCachingFactorizer() throws InterruptedException {
     UnsafeCachingFactorizer factorizer = new UnsafeCachingFactorizer();
 
-    submitForExecutionForNumberOfTimes(() -> {
-      String number = String.valueOf(ThreadLocalRandom.current().nextInt(10, 12 + 1));
-      System.out.println("Generated " + number);
-      ServletRequest request = ServletHelper.newServletRequestWithFactorNumber(new BigInteger(number));
-      ServletResponse response = ServletHelper.newServletResponse();
+    List<Result> results = doGivenNumberOfConcurrentInitializations(factorizer, NUMBER_OF_ITERATIONS);
 
-      factorizer.service(request, response);
-      BigInteger[] factors = ServletHelper.extractFactors(response);
-
-      assertThat(factors).isEqualTo(new BigInteger[] {new BigInteger("44")});
-      return true;
-    }, NUMBER_OF_ITERATIONS);
+    results.forEach(res -> res.assertThatInputMatchesResult());
   }
 
-  class Result {
+  private List<Result> doGivenNumberOfConcurrentInitializations(UnsafeCachingFactorizer factorizer, int iterations) throws InterruptedException {
+    List<Result> createdInstances =
+        submitForExecutionForNumberOfTimes(() -> {
+          String number = String.valueOf(ThreadLocalRandom.current().nextInt(10, 12 + 1));
+          ServletRequest request = ServletHelper.newServletRequestWithFactorNumber(new BigInteger(number));
+          ServletResponse response = ServletHelper.newServletResponse();
+
+          factorizer.service(request, response);
+          BigInteger[] factors = ServletHelper.extractFactors(response);
+          return new Result(new BigInteger(number), factors);
+        }, iterations).stream()
+            .map(this::toResult)
+            .collect(Collectors.toList());
+    return createdInstances;
+  }
+
+  // TBD probably it might be shared
+  private Result toResult(Future<Result> future) {
+    try {
+      return future.get();
+    } catch (ExecutionException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static class Result {
 
     private final BigInteger factorNumber;
     private final BigInteger[] factors;
