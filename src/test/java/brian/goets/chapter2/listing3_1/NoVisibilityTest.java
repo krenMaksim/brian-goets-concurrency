25f5d362a0b1b603f933100d33ded6f37b0faa6b
krenMaksim <krenmaba@gmail.com>
2024-10-14 09:02:00 +0300

    done with NoVisibilityTest

--- src/test/java/brian/goets/chapter2/listing3_1/NoVisibilityTest.java
+++ src/test/java/brian/goets/chapter2/listing3_1/NoVisibilityTest.java
@@ -1,115 +1,36 @@
 package brian.goets.chapter2.listing3_1;
 
-import brian.goets.test.util.TaskIterator;
 import org.junit.jupiter.api.RepeatedTest;
 
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/*
+  In both cases I could not reproduce visibility problem described in the book.
+  What might be considered as another prove that concurrency issues difficult to spot.
+ */
 class NoVisibilityTest {
+
+  private static final int NUMBER_OF_ITERATIONS = 10_000;
 
   @RepeatedTest(100)
   void tryToSetStaticVariables() throws InterruptedException {
-    Thread thread = new NoVisibility.ReaderThread();
-    thread.start();
-    NoVisibility.number = 42;
-    NoVisibility.ready = true;
-    thread.join();
-  }
-
-  @RepeatedTest(100)
-  void tryToSetStaticVariables2() throws InterruptedException {
-    //    new NoVisibility.ReaderThread().start();
-
-    TaskIterator.submitForExecutionNumberOfTimes(() -> {
-      new NoVisibility.ReaderThread().run();
-      return true;
-    }, 1000);
-
-    NoVisibility.number = 42;
-    NoVisibility.ready = true;
-  }
-
-  @RepeatedTest(100)
-  void tryToSetStaticVariables3() throws InterruptedException {
-    //    new NoVisibility.ReaderThread().start();
-
-    Runnable ru = () -> {
-      try {
-        TaskIterator.submitForExecutionNumberOfTimes(() -> {
-          new NoVisibility.ReaderThread().run();
-          return true;
-        }, 1000);
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
-    };
-    Thread thread = new Thread(ru);
-    thread.start();
-
-    NoVisibility.number = 42;
-    NoVisibility.ready = true;
-    System.out.println("ddddd");
-    thread.join();
-  }
-
-  // TBD mention that I could not reproduce expected issue via test
-  @RepeatedTest(100)
-  void tryToSetStaticVariables4() throws InterruptedException {
     ExecutorService exec = Executors.newFixedThreadPool(20);
 
-    System.out.println("execute");
-    IntStream.rangeClosed(1, 1000)
+    IntStream.rangeClosed(1, NUMBER_OF_ITERATIONS)
         .forEach(i -> exec.execute(new NoVisibility.ReaderThread()));
-    System.out.println("after execute");
-
-    TimeUnit.NANOSECONDS.sleep(100);
 
     NoVisibility.number = 42;
     NoVisibility.ready = true;
-    System.out.println("ddddd");
 
     exec.shutdown();
     exec.awaitTermination(30, TimeUnit.SECONDS);
   }
 
-  @RepeatedTest(10)
-  void tryToSetStaticVariables5() throws InterruptedException {
-    ExecutorService exec = Executors.newFixedThreadPool(10);
-
-    Callable<Boolean> task = () -> {
-      new NoVisibility.ReaderThread().run();
-      return true;
-    };
-
-    Callable<Boolean> task2 = () -> {
-      NoVisibility.number = 42;
-      NoVisibility.ready = true;
-      return true;
-    };
-
-    List<Callable<Boolean>> tasks = IntStream.rangeClosed(1, 9)
-        .mapToObj(i -> task)
-        .collect(Collectors.toList());
-
-    tasks.add(task2);
-
-    System.out.println("execute");
-    exec.invokeAll(tasks);
-    System.out.println("after execute");
-
-    exec.shutdown();
-    exec.awaitTermination(30, TimeUnit.SECONDS);
-  }
-
-  // TBD it might be option 2
-  @RepeatedTest(10_000)
-  void tryToSetStaticVariables7() {
+  @RepeatedTest(NUMBER_OF_ITERATIONS)
+  void executeMainMethod() {
     NoVisibility.main(null);
   }
 }
