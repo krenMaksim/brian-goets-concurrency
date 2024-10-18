55f105c8dfb7c546731c5c0439b26bc2ffb01fc4
krenMaksim <krenmaba@gmail.com>
2024-10-18 07:00:19 +0300

    done TestHarnessTest

--- src/test/java/brian/goets/chapter5/listing5_11/TestHarnessTest.java
+++ src/test/java/brian/goets/chapter5/listing5_11/TestHarnessTest.java
@@ -1,25 +1,36 @@
 package brian.goets.chapter5.listing5_11;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 class TestHarnessTest {
 
+  private static final int NUMBER_THREADS = 50;
+
+  @BeforeEach
+  void setUp() {
+    System.out.println("Main thread started");
+  }
+
   @Test
-  void timeTasks() throws InterruptedException {
+  void timeTasks() {
     TestHarness testHarness = new TestHarness();
 
-    int numberThreads = 50;
+    assertThatCode(() -> testHarness.timeTasks(NUMBER_THREADS, this::doSomeJob))
+        .doesNotThrowAnyException();
+  }
 
-    Runnable task = () -> {
-      for (int i = 1; i <= 5; i++) {
-        System.out.println(String.format("i=%d; %s", i, Thread.currentThread().getName()));
-      }
-    };
+  private void doSomeJob() {
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(String.format("i=%d; %s", i, Thread.currentThread().getName()));
+    }
+  }
 
-    System.out.println("main thread start");
-
-    testHarness.timeTasks(numberThreads, task);
-
-    System.out.println("main thread finish");
+  @AfterEach
+  void tearDown() {
+    System.out.println("Main thread finished");
   }
 }
