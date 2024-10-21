64a305c64310adc1b48336ffe4fdeb13baadbfc8
krenMaksim <krenmaba@gmail.com>
2024-10-21 06:43:41 +0300

    updated Memoizer3

--- src/main/java/brian/goets/chapter5/listing5_16/Memoizer3.java
+++ src/main/java/brian/goets/chapter5/listing5_16/Memoizer3.java
@@ -1,7 +1,5 @@
 package brian.goets.chapter5.listing5_16;
 
-import brian.goets.chapter5.listing5_13.LaunderThrowable;
-
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,26 +7,24 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
+import static brian.goets.chapter5.listing5_13.LaunderThrowable.launderThrowable;
+
 class Memoizer3<A, V> implements Computable<A, V> {
 
-  private final Map<A, Future<V>> cache = new ConcurrentHashMap<A, Future<V>>();
+  private final Map<A, Future<V>> cache;
   private final Computable<A, V> c;
 
   public Memoizer3(Computable<A, V> c) {
     this.c = c;
+    this.cache = new ConcurrentHashMap<>();
   }
 
   @Override
   public V compute(final A arg) throws InterruptedException {
     Future<V> f = cache.get(arg);
     if (f == null) {
-      Callable<V> eval = new Callable<V>() {
-        @Override
-        public V call() throws InterruptedException {
-          return c.compute(arg);
-        }
-      };
-      FutureTask<V> ft = new FutureTask<V>(eval);
+      Callable<V> eval = () -> c.compute(arg);
+      FutureTask<V> ft = new FutureTask<>(eval);
       f = ft;
       cache.put(arg, ft);
       ft.run(); // call to c.compute happens here
@@ -36,7 +32,7 @@ class Memoizer3<A, V> implements Computable<A, V> {
     try {
       return f.get();
     } catch (ExecutionException e) {
-      throw LaunderThrowable.launderThrowable(e.getCause());
+      throw launderThrowable(e.getCause());
     }
   }
 }
