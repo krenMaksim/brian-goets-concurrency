2f6490c2e62c9c57882aaaaa6dd2d70b8a887d40
krenMaksim <krenmaba@gmail.com>
2024-10-30 07:08:33 +0300

    updated SingleThreadWebServer

--- src/main/java/brian/goets/chapter6/listing6_1/SingleThreadWebServer.java
+++ src/main/java/brian/goets/chapter6/listing6_1/SingleThreadWebServer.java
@@ -6,30 +6,31 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 class SingleThreadWebServer {
-    public static void main(String[] args) throws IOException {
-        @SuppressWarnings("resource")
-        ServerSocket socket = new ServerSocket(8081);
-        while (true) {
-            try (Socket connection = socket.accept()) {
-                handleRequest(connection);
-            }
-        }
-    }
 
-    private static void handleRequest(Socket connection) {
-        try {
-            String content = convert(connection.getInputStream());
-            System.out.println(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void main(String[] args) throws IOException {
+    @SuppressWarnings("resource")
+    ServerSocket socket = new ServerSocket(8081);
+    while (true) {
+      try (Socket connection = socket.accept()) {
+        handleRequest(connection);
+      }
     }
+  }
 
-    private static String convert(InputStream inputStream) throws IOException {
-        StringBuffer input = new StringBuffer();
-        while (inputStream.available() > 0) {
-            input.append((char) inputStream.read());
-        }
-        return input.toString();
+  private static void handleRequest(Socket connection) {
+    try {
+      String content = convert(connection.getInputStream());
+      System.out.println(content);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  private static String convert(InputStream inputStream) throws IOException {
+    StringBuilder input = new StringBuilder();
+    while (inputStream.available() > 0) {
+      input.append((char) inputStream.read());
+    }
+    return input.toString();
+  }
 }
