6e4ae4562d6c891747ba739520d2564203ec9b5e
krenMaksim <krenmaba@gmail.com>
2024-11-01 10:39:33 +0300

    done FutureRenderer

--- src/main/java/brian/goets/chapter6/listing6_10/SingleThreadRenderer.java
+++ src/main/java/brian/goets/chapter6/listing6_10/SingleThreadRenderer.java
@@ -2,9 +2,9 @@ package brian.goets.chapter6.listing6_10;
 
 class SingleThreadRenderer extends Renderer {
 
-  public void renderPage(CharSequence source) {
-    renderText(source);
-    scanForImageInfo(source).stream()
+  public void renderPage(CharSequence page) {
+    renderText(page);
+    scanForImageInfo(page).stream()
         .map(ImageInfo::downloadImage)
         .forEach(this::renderImage);
   }
--- src/main/java/brian/goets/chapter6/listing6_13/FutureRenderer.java
+++ src/main/java/brian/goets/chapter6/listing6_13/FutureRenderer.java
@@ -2,21 +2,49 @@ package brian.goets.chapter6.listing6_13;
 
 import brian.goets.chapter6.listing6_10.Renderer;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+import static brian.goets.chapter5.listing5_13.LaunderThrowable.launderThrowable;
 
 public class FutureRenderer extends Renderer {
 
-  private final ExecutorService executorService;
+  private final ExecutorService executor;
 
   public FutureRenderer() {
-    this.executorService = Executors.newFixedThreadPool(10);
+    this.executor = Executors.newFixedThreadPool(10);
   }
 
-  public void renderPage(CharSequence source) {
-    renderText(source);
-    scanForImageInfo(source).stream()
-        .map(ImageInfo::downloadImage)
+  public void renderPage(CharSequence page) {
+    Future<List<ImageData>> downloadingImagesTask = executor.submit(() -> downloadImages(page));
+
+    renderText(page);
+
+    getTaskResult(downloadingImagesTask)
         .forEach(this::renderImage);
+  }
+
+  private List<ImageData> downloadImages(CharSequence source) {
+    return scanForImageInfo(source).stream()
+        .map(ImageInfo::downloadImage)
+        .collect(Collectors.toUnmodifiableList());
+  }
+
+  private static List<ImageData> getTaskResult(Future<List<ImageData>> task) {
+    try {
+      return task.get();
+    } catch (InterruptedException e) {
+      // Re-assert the thread’s interrupted status
+      Thread.currentThread().interrupt();
+      // We don’t need the result, so cancel the task too
+      task.cancel(true);
+      return List.of();
+    } catch (ExecutionException e) {
+      throw launderThrowable(e.getCause());
+    }
   }
 }
