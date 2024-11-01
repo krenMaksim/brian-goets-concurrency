e521973d2251e6bd1d40afdfaf5321b5b6acf8ac
krenMaksim <krenmaba@gmail.com>
2024-11-01 10:19:12 +0300

    allocated renderPageViaFutureRenderer

--- src/main/java/brian/goets/chapter6/listing6_13/FutureRenderer.java
+++ src/main/java/brian/goets/chapter6/listing6_13/FutureRenderer.java
@@ -2,7 +2,16 @@ package brian.goets.chapter6.listing6_13;
 
 import brian.goets.chapter6.listing6_10.Renderer;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class FutureRenderer extends Renderer {
+
+  private final ExecutorService executorService;
+
+  public FutureRenderer() {
+    this.executorService = Executors.newFixedThreadPool(10);
+  }
 
   public void renderPage(CharSequence source) {
     renderText(source);
--- src/test/java/brian/goets/chapter6/listing6_10/RendererTest.java
+++ src/test/java/brian/goets/chapter6/listing6_10/RendererTest.java
@@ -1,16 +1,25 @@
 package brian.goets.chapter6.listing6_10;
 
+import brian.goets.chapter6.listing6_13.FutureRenderer;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 class RendererTest {
 
+  private static final String PAGE = "Some page";
+
   @Test
-  void renderPage() {
-    String page = "Some page";
+  void renderPageViaSingleThreadRenderer() {
     Renderer renderer = new SingleThreadRenderer();
 
-    assertThatCode(() -> renderer.renderPage(page)).doesNotThrowAnyException();
+    assertThatCode(() -> renderer.renderPage(PAGE)).doesNotThrowAnyException();
+  }
+
+  @Test
+  void renderPageViaFutureRenderer() {
+    Renderer renderer = new FutureRenderer();
+
+    assertThatCode(() -> renderer.renderPage(PAGE)).doesNotThrowAnyException();
   }
 }
