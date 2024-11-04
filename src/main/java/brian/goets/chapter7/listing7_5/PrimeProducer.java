3a6a5e4e8c80fdef64ac343ef16f78b33ce8f14d
krenMaksim <krenmaba@gmail.com>
2024-11-04 06:49:14 +0300

    Updated PrimeProducer

--- src/main/java/brian/goets/chapter7/listing7_5/PrimeProducer.java
+++ src/main/java/brian/goets/chapter7/listing7_5/PrimeProducer.java
@@ -19,7 +19,12 @@ class PrimeProducer extends Thread {
         queue.put(p = p.nextProbablePrime());
       }
     } catch (InterruptedException consumed) {
-      /* Allow thread to exit */
+      /* Allow thread to exit
+       * PrimeProducer swallows the interrupt, but does so with the knowledge that the thread is about to terminate and
+       * that therefore there is no code higher up on the call stack that needs to know about the interruption.
+       * Most code does not know what thread it will run in and so should preserve the interrupted status.
+       *
+       *  */
     }
   }
 
