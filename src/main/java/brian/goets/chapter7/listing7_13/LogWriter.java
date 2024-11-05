91ddfc24544baa99f3d890b8173b148eb3552d18
krenMaksim <krenmaba@gmail.com>
2024-11-05 07:04:04 +0300

    done LogWriter

--- src/main/java/brian/goets/chapter7/listing7_13/LogWriter.java
+++ src/main/java/brian/goets/chapter7/listing7_13/LogWriter.java
@@ -5,40 +5,43 @@ import java.io.Writer;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class LogWriter {
-    private final BlockingQueue<String> queue;
-    private final LoggerThread logger;
-    private static final int CAPACITY = 1000;
+class LogWriter {
 
-    public LogWriter(Writer writer) {
-        this.queue = new LinkedBlockingQueue<String>(CAPACITY);
-        this.logger = new LoggerThread(writer);
+  private static final int CAPACITY = 1000;
+
+  private final BlockingQueue<String> queue;
+  private final LoggerThread logger;
+
+  public LogWriter(Writer writer) {
+    this.queue = new LinkedBlockingQueue<>(CAPACITY);
+    this.logger = new LoggerThread(writer);
+  }
+
+  public void start() {
+    logger.start();
+  }
+
+  public void log(String msg) throws InterruptedException {
+    queue.put(msg);
+  }
+
+  private class LoggerThread extends Thread {
+
+    private final PrintWriter writer;
+
+    public LoggerThread(Writer writer) {
+      this.writer = new PrintWriter(writer, true);
     }
 
-    public void start() {
-        logger.start();
-    }
-
-    public void log(String msg) throws InterruptedException {
-        queue.put(msg);
-    }
-
-    private class LoggerThread extends Thread {
-        private final PrintWriter writer;
-
-        public LoggerThread(Writer writer) {
-            this.writer = new PrintWriter(writer, true); // autoflush
+    public void run() {
+      try {
+        while (true) {
+          writer.println(queue.take());
         }
-
-        public void run() {
-            try {
-                while (true) {
-                    writer.println(queue.take());
-                }
-            } catch (InterruptedException ignored) {
-            } finally {
-                writer.close();
-            }
-        }
+      } catch (InterruptedException ignored) {
+      } finally {
+        writer.close();
+      }
     }
+  }
 }
