9bd7f72e64caa2ab2ee843e528c78da399f8441e
krenMaksim <krenmaba@gmail.com>
2024-11-06 06:52:33 +0300

    added IndexingService

--- src/main/java/brian/goets/chapter7/listing7_17/IndexingService.java
+++ src/main/java/brian/goets/chapter7/listing7_17/IndexingService.java
@@ -5,88 +5,89 @@ import java.io.FileFilter;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class IndexingService {
-    private static final int CAPACITY = 1000;
-    private static final File POISON = new File("");
+class IndexingService {
 
-    private final IndexerThread consumer = new IndexerThread();
-    private final CrawlerThread producer = new CrawlerThread();
-    private final BlockingQueue<File> queue;
-    private final FileFilter fileFilter;
-    private final File root;
+  private static final int CAPACITY = 1000;
+  private static final File POISON = new File("");
 
-    public IndexingService(File root, final FileFilter fileFilter) {
-        this.root = root;
-        this.queue = new LinkedBlockingQueue<File>(CAPACITY);
-        this.fileFilter = new FileFilter() {
-            public boolean accept(File f) {
-                return f.isDirectory() || fileFilter.accept(f);
-            }
-        };
-    }
+  private final IndexerThread consumer = new IndexerThread();
+  private final CrawlerThread producer = new CrawlerThread();
+  private final BlockingQueue<File> queue;
+  private final FileFilter fileFilter;
+  private final File root;
 
-    private boolean alreadyIndexed(File f) {
-        return false;
-    }
+  public IndexingService(File root, final FileFilter fileFilter) {
+    this.root = root;
+    this.queue = new LinkedBlockingQueue<>(CAPACITY);
+    this.fileFilter = f -> f.isDirectory() || fileFilter.accept(f);
+  }
 
-    class CrawlerThread extends Thread {
-        public void run() {
-            try {
-                crawl(root);
-            } catch (InterruptedException e) { /* fall through */
-            } finally {
-                while (true) {
-                    try {
-                        queue.put(POISON);
-                        break;
-                    } catch (InterruptedException e1) { /* retry */
-                    }
-                }
-            }
+  private boolean alreadyIndexed(File f) {
+    return false;
+  }
+
+  class CrawlerThread extends Thread {
+
+    public void run() {
+      try {
+        crawl(root);
+      } catch (InterruptedException e) { /* fall through */
+      } finally {
+        while (true) {
+          try {
+            queue.put(POISON);
+            break;
+          } catch (InterruptedException e1) { /* retry */
+          }
         }
+      }
+    }
 
-        private void crawl(File root) throws InterruptedException {
-            File[] entries = root.listFiles(fileFilter);
-            if (entries != null) {
-                for (File entry : entries) {
-                    if (entry.isDirectory())
-                        crawl(entry);
-                    else if (!alreadyIndexed(entry))
-                        queue.put(entry);
-                }
-            }
+    private void crawl(File root) throws InterruptedException {
+      File[] entries = root.listFiles(fileFilter);
+      if (entries != null) {
+        for (File entry : entries) {
+          if (entry.isDirectory()) {
+            crawl(entry);
+          } else if (!alreadyIndexed(entry)) {
+            queue.put(entry);
+          }
         }
+      }
     }
+  }
 
-    class IndexerThread extends Thread {
-        public void run() {
-            try {
-                while (true) {
-                    File file = queue.take();
-                    if (file == POISON)
-                        break;
-                    else
-                        indexFile(file);
-                }
-            } catch (InterruptedException consumed) {
-            }
+  class IndexerThread extends Thread {
+
+    public void run() {
+      try {
+        while (true) {
+          File file = queue.take();
+          if (file == POISON) {
+            break;
+          } else {
+            indexFile(file);
+          }
         }
-
-        public void indexFile(File file) {
-            /* ... */
-        };
+      } catch (InterruptedException consumed) {
+      }
     }
 
-    public void start() {
-        producer.start();
-        consumer.start();
+    public void indexFile(File file) {
+      /* ... */
     }
+  }
 
-    public void stop() {
-        producer.interrupt();
-    }
+  public void start() {
+    producer.start();
+    consumer.start();
+  }
 
-    public void awaitTermination() throws InterruptedException {
-        consumer.join();
-    }
+  public void stop() {
+    producer.interrupt();
+  }
+
+  public void awaitTermination() throws InterruptedException {
+    consumer.join();
+  }
 }
